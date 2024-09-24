c63f6c0dadb1319f4ddd07d6d086f1ad212e5176
goustg62@gmail.com <goustg62@gmail.com>
2024-09-24 13:03:44 +0300

    Resolved merge conflicts

---
+++ mysql/mysql_main.py
@@ -0,0 +1,39 @@
+import pymysql
+import pymysql.cursors
+from mysql_config import host, user, password, db_name
+
+def execute_db(task):
+    try:
+        conn = pymysql.connect(
+            host=host,
+            port=3306,
+            user=user,
+            password=password,
+            database="root",
+            cursorclass=pymysql.cursors.DictCursor
+        )
+        print("Connected to the database successfully...")
+        with conn.cursor() as cur:
+            cur.execute(task)
+        conn.commit()
+    except Exception as e:
+        print("Error in connection...")
+        print(e)
+        return False
+    finally:
+        conn.close()
+
+
+if __name__ == "__main__":
+    execute_db(
+
+                """
+                   CREATE TABLE IF NOT EXISTS kinopoisk (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       code INT NOT NULL,
+                       headline VARCHAR(255) NOT NULL,
+                       description TEXT,
+                       photo BLOB
+                   );
+                """
+    )    
